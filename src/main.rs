f5cf857b329323d5703822caf9d5a0020142aafb
zehua hao <49864959+Harzva@users.noreply.github.com>
2026-05-17 19:28:21 +0800

    Restore iOS plist and publish v0.2.6

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result<()> {
     };
 
     eframe::run_native(
-        "GitMarket v0.2.5",
+        "GitMarket v0.2.6",
         options,
         Box::new(|_cc| Ok(Box::new(GitMarketApp::default()))),
     )
